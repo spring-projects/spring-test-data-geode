da4025af026c682332070a0170bc006fe2c0ece5
John Blum <jblum@pivotal.io>
2018-05-27 01:36:48 -0700

    Add logging support.

--- spring-test-data-geode/src/main/java/org/springframework/data/gemfire/tests/integration/config/ClientServerIntegrationTestsConfiguration.java
+++ spring-test-data-geode/src/main/java/org/springframework/data/gemfire/tests/integration/config/ClientServerIntegrationTestsConfiguration.java
@@ -22,6 +22,8 @@ import java.util.Collections;
 
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.Pool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -45,6 +47,12 @@ import org.springframework.data.gemfire.support.ConnectionEndpoint;
 @Configuration
 @SuppressWarnings("unused")
 public class ClientServerIntegrationTestsConfiguration {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	protected Logger getLogger() {
+		return this.logger;
+	}
 
 	@Bean
 	ClientCacheConfigurer clientCachePoolPortConfigurer(
