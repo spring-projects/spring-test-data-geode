2d72115fa94d84d5240314f3f4231fb31d6aef39
John Blum <jblum@pivotal.io>
2019-09-09 18:43:42 -0700

    Move GemFireObjectCreationTriggeredByGemFirePropertyConfigurationIntegrationTests to org.springframework.data.gemfire.tests.

--- spring-data-geode-test/src/test/java/org/springframework/data/gemfire/tests/GemFireObjectCreationTriggeredByGemFirePropertyConfigurationIntegrationTests.java
+++ spring-data-geode-test/src/test/java/org/springframework/data/gemfire/tests/GemFireObjectCreationTriggeredByGemFirePropertyConfigurationIntegrationTests.java
@@ -13,7 +13,7 @@
  *  or implied. See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package org.springframework.data.gemfire.tests.mock;
+package org.springframework.data.gemfire.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
