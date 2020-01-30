d31b2bcad216d71741725a9e7767d60d5ff73ce8
Patrick Johnson <pjohnson@pivotal.io>
2020-01-29 16:39:51 -0800

    Add functionality to TestSecurityManager.

--- spring-data-geode-test/src/main/java/org/springframework/data/gemfire/tests/objects/geode/security/TestSecurityManager.java
+++ spring-data-geode-test/src/main/java/org/springframework/data/gemfire/tests/objects/geode/security/TestSecurityManager.java
@@ -15,13 +15,22 @@
  */
 package org.springframework.data.gemfire.tests.objects.geode.security;
 
+import static org.springframework.data.gemfire.config.annotation.support.AutoConfiguredAuthenticationInitializer.SECURITY_PASSWORD_PROPERTY;
+import static org.springframework.data.gemfire.config.annotation.support.AutoConfiguredAuthenticationInitializer.SECURITY_USERNAME_PROPERTY;
+import static org.springframework.data.gemfire.util.RuntimeExceptionFactory.newIllegalArgumentException;
 import static org.springframework.data.gemfire.util.RuntimeExceptionFactory.newIllegalStateException;
 
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.geode.security.AuthenticationFailedException;
+import org.springframework.util.StringUtils;
 
 /**
  * Test {@link org.apache.geode.security.SecurityManager} implementation used for testing purposes (only).
@@ -36,6 +45,11 @@ public class TestSecurityManager implements org.apache.geode.security.SecurityMa
 
 	private static final AtomicReference<TestSecurityManager> instance = new AtomicReference<>(null);
 
+	public static final String SECURITY_USERNAME = "testUser";
+	public static final String SECURITY_PASSWORD = "&t35t9@55w0rd!";
+
+	private final ConcurrentMap<String, String> authorizedUsers;
+
 	public static TestSecurityManager getInstance() {
 
 		return Optional.ofNullable(instance.get())
@@ -43,11 +57,52 @@ public class TestSecurityManager implements org.apache.geode.security.SecurityMa
 	}
 
 	public TestSecurityManager() {
+		this.authorizedUsers = new ConcurrentHashMap<>();
+		this.authorizedUsers.putIfAbsent(SECURITY_USERNAME, SECURITY_PASSWORD);
 		instance.compareAndSet(null, this);
 	}
 
 	@Override
 	public Object authenticate(Properties credentials) throws AuthenticationFailedException {
-		throw new UnsupportedOperationException("Not Implemented");
+		String username = credentials.getProperty(SECURITY_USERNAME_PROPERTY);
+		String password = credentials.getProperty(SECURITY_PASSWORD_PROPERTY);
+
+		return Optional.ofNullable(identify(username, password)).orElseThrow(() ->
+				new AuthenticationFailedException(String.format("User [%s] is not authorized", username)));	}
+
+	private Principal identify(String username, String password) {
+		return (isIdentified(username, password) ? TestPrincipal.newPrincipal(username) : null);
+	}
+
+	private boolean isIdentified(String username, String password) {
+
+		return Optional.ofNullable(username)
+				.filter(StringUtils::hasText)
+				.map(user -> getAuthorizedUsers().get(user))
+				.map(userPassword -> userPassword.equals(password))
+				.orElse(false);
+	}
+
+	protected Map<String, String> getAuthorizedUsers() {
+		return Collections.unmodifiableMap(this.authorizedUsers);
+	}
+
+	public static final class TestPrincipal implements java.security.Principal, java.io.Serializable {
+
+		private final String name;
+
+		public static TestPrincipal newPrincipal(String username) {
+			return new TestPrincipal(username);
+		}
+
+		public TestPrincipal(String name) {
+			this.name = Optional.ofNullable(name).filter(StringUtils::hasText)
+					.orElseThrow(() -> newIllegalArgumentException("Name is required"));
+		}
+
+		@Override
+		public String getName() {
+			return this.name;
+		}
 	}
 }
