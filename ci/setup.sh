73a1cf2d11febbf55437191c983cb5c0cd5fd33f
John Blum <jblum@vmware.com>
2022-05-04 11:08:34 -0700

    Add CI build shell scripts.

---
+++ ci/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+# User ID 1001 is "jenkins"
+# Group ID 1001 is "jenkins"
+# Syntax: `chown -R userId:groupId .`
+
+echo "Logged into Jenkins CI as user [$USER] with home directory [$HOME] in the current working directory [$PWD]"
+chown -R 1001:1001 .
+#echo "Logging into Docker..."
+#docker login --username ${DOCKER_HUB_USR} --password ${DOCKER_HUB_PSW}
+exit 0
